03a015e49e3aba72864d2bc23a4a34a3c7c72c41
theassembly <32713072+The-Assembly@users.noreply.github.com>
2019-11-02 02:37:00 +0400

    Add files via upload

---
+++ DisplayPrintFunction_OLED/DisplayPrintFunction_OLED.ino
@@ -0,0 +1,28 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+Adafruit_SSD1306 display(-1);
+
+void setup()   
+{                
+  // initialize with the I2C addr 0x3C
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  
+
+  // Clear the buffer.
+  display.clearDisplay();
+
+  // Display Text
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(35,10);
+  display.println("Hello world!");
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+
+}
+
+void loop() {}
